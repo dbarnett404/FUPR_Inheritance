80e7754439f2cafceba994ae9df9eb4bc7a6ce16
db486 <db486@sussex.ac.uk>
2026-01-27 10:30:17 +0000

    Hello

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Buddy", 3, "Golden Retriever");
+        Dog myDog = new Dog("Shadow", 4, "Whippet Greyhound");
         myDog.eat();    // Inherited from Animal
         myDog.sleep();  // Inherited from Animal
         myDog.bark();   // Specific to Dog
